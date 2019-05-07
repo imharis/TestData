d3e22dac6a6649da5683eecaeb46780403156a55
imharis <haris.codingpixcel@gmail.com>
2019-05-07 09:12:41 +0500

    First commit

---
+++ src/TestData.php
@@ -0,0 +1,14 @@
+<?php
+namespace imharis\TestData;
+
+class TestData
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+}
